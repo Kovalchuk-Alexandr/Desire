c83463824aae8c864bcec5e77a646db65032bb9e
Kovalchuk-Alexandr <mo250568kaa@gmail.com>
2025-02-06 10:14:25 +0200

    contact map +

--- src/js/index.js
+++ src/js/index.js
@@ -52,5 +52,28 @@ if (containerEl) {
 // console.log(`URI: ${document.documentURI}`);
 // console.log(`URL: ${document.title}`);
 
+function initMap() {
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 20,
+        // 37.797822, -122.422611
+        center: { lat: 37.797822, lng: -122.422611 },
+        mapTypeControl: true,
+        mapTypeControlOptions: {
+            style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+            position: google.maps.ControlPosition.TOP_CENTER,
+        },
+        zoomControl: true,
+        zoomControlOptions: {
+            position: google.maps.ControlPosition.LEFT_CENTER,
+        },
+        scaleControl: true,
+        streetViewControl: true,
+        streetViewControlOptions: {
+            position: google.maps.ControlPosition.LEFT_BOTTOM,
+        },
+        fullscreenControl: true,
+    });
+}
 
+window.initMap = initMap;
 
